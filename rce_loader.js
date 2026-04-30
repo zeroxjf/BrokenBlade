cf6f3cb3ba08793c347ba649f701a7471bc74625
zeroxjf <johnnycfranks@gmail.com>
2026-04-30 10:30:28 -0400

    Harden PE handoff and failure reporting

--- rce_loader.js
+++ rce_loader.js
@@ -75,7 +75,7 @@ function print(x, reportError = false, dumphex = false) {
             source: 'webcontent',
             reportError: !!reportError,
             dumphex: !!dumphex
-        }, location.origin);
+        }, '*');
     } catch (e) {}
     if (!SERVER_LOG && !reportError) return;
     let obj = {
@@ -299,7 +299,7 @@ let workerBlobUrl = URL.createObjectURL(workerBlob);
                 const token = (data.token || "").toString();
                 if (token.length > 0) {
                     try { sessionStorage.setItem('lightsaber_token', token); } catch (e) {}
-                    try { window.parent.postMessage({ type: 'lightsaber_token', token: token }, location.origin); } catch (e) {}
+                    try { window.parent.postMessage({ type: 'lightsaber_token', token: token }, '*'); } catch (e) {}
                 }
                 break;
             }
@@ -313,7 +313,7 @@ let workerBlobUrl = URL.createObjectURL(workerBlob);
                             source: 'worker',
                             reportError: !!data.reportError,
                             dumphex: !!data.dumphex
-                        }, location.origin);
+                        }, '*');
                     } catch(e) {}
                 }
                 break;
